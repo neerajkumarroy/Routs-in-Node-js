0be67a6839f5d59961a7a994ea1629b39057fffe
Neeraj Kumar <148259510+neerajkumarroy@users.noreply.github.com>
2023-11-17 15:58:30 +0530

    oo
    To use the colors npm package to add color to console output in Node.js, you can install the package and then use it in your code like this:

---
+++ col.js
@@ -0,0 +1,8 @@
+var colors = require('colors');
+ 
+console.log('hello'.green); 
+console.log('i like cake and pies'.underline.red) 
+console.log('inverse the color'.inverse);
+console.log('HELLO, This is Neeraj!'.rainbow); 
+console.log('Today is the owsome day'.trap); 
+ 
